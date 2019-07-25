7a76575865e9d9941c60c9d9f18f5e4247bb57f6
cp0418 <s5902041610053@email.kmutnb.ac.th>
2019-07-25 15:39:05 +0700

    assignment4

---
+++ assignment4/main.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__) #__name__ = filename
+@app.route('/')
+def index():
+    return "hello World"
+
+if __name__=='__main__':
+    app.run() #app is object from flask
