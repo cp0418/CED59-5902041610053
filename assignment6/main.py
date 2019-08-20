9c58c2f622a5154f3dc7e92b7749bb57b91d02a8
cp0418 <s5902041610053@email.kmutnb.ac.th>
2019-08-20 20:10:05 +0700

    Update Assignment 7

--- assignment6/main.py
+++ assignment6/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 
-
+pip
 
 @app.route('/')
 def student():
--- assignment7/main.py
+++ assignment7/main.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template, request
+from wtforms import StringField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
 
-
+class RegisForm(FlaskForm):
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, message="Password at least 8 characters!")])
+    name = StringField("name", validators=[InputRequired()])
+    last = StringField("last", validators=[InputRequired()])
+    email = StringField("Email",  [InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
 
 
 @app.route('/')
 def student():
+    form = RegisForm()
+    return render_template('index.html', form=form)
 
-   return render_template('index.html')
 
-
-@app.route('/', methods=["POST"])
-def register():
-       return "User already exists"
-
+@app.route('/regis', methods=["GET", "POST"])
+def regis():
+    form = RegisForm()
+    if form.validate_on_submit():
+        return "The form has been submitted"
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
